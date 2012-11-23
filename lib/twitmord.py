fe33c0664d9af0276f1922d4b2bb924f0841a191
orithena <maligree@gmx.de>
2012-11-23 16:10:28 +0100

    some minor bugfixes and enhancements

--- lib/twitmord.py
+++ lib/twitmord.py
@@ -3,17 +3,23 @@
 import twython
 
 def twit(msg):
-  try:
-    client = twython.Twython(app_key='TODO:insert app key', app_secret='TODO:insert app secret', oauth_token='TODO:insert token', oauth_token_secret='TODO:insert token secret')  
+  try: # get tokens from https://dev.twitter.com/
+    client = twython.Twython(app_key='TODO:insert app key', app_secret='TODO: insert app secret', oauth_token='TODO:insert oauth token', oauth_token_secret='TODO:insert oauth secret')  
     client.updateStatus(status=msg)
   except:
-    pass
+    pass #if there is an error while tweeting, it can't be solved by the user, so he does not need an error message
 
 def killmsg(victim):
-  msg = u"Mörderspiel #%s: %s erlegte %s in Kreis %s: %s" % (victim.player.game.id, victim.killedby.killer.player.name, victim.player.name, victim.round.name, victim.killedby.reason)
+  if len(victim.player.game.rounds) > 1:
+    msg = u"Mörderspiel #%s: %s erlegte %s in Kreis %s: %s" % (victim.player.game.id, victim.killedby.killer.player.name, victim.player.name, victim.round.name, victim.killedby.reason)
+  else:
+    msg = u"Mörderspiel #%s: %s erlegte %s: %s" % (victim.player.game.id, victim.killedby.killer.player.name, victim.player.name, victim.killedby.reason)
   return msg[:140]
 
 def twitkill(victim):
-  msg = killmsg(victim)
-  twit(msg[:140])
+  try:
+    msg = killmsg(victim)
+    twit(msg[:140])
+  except:
+    pass #if killmsg produces an error, it does not know how to produce a message for this situation
 
